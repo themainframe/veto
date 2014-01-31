8dfb1d6d183139ce878c36432ead088a5ea30242
Damien Walsh <me@damow.net>
2014-01-31 21:21:44 +0000

    Add test root, bootstrapper & configuration file.

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Veto.
+ * PHP Microframework.
+ *
+ * @author Damien Walsh <me@damow.net>
+ * @copyright Damien Walsh 2013-2014
+ * @version 0.1
+ * @package veto
+ */
+
+$loader = require __DIR__ . "/../vendor/autoload.php";
+$loader->add('Veto\\', __DIR__);
