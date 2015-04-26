f9411719911be776e317e7b06ec12f7c9fbe0cc7
Damien Walsh <me@damow.net>
2015-04-26 23:53:52 +0100

    Fix: Improve test bootstrap script to handle testing a Veto installed as a dependency.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,5 +9,27 @@
  * @package veto
  */
 
-$loader = require __DIR__ . "/../vendor/autoload.php";
+/*
+ * We will try to load an autoloader from different paths for two different cases:
+ *
+ *  - Veto may be installed _not_ as a dependency of another project.
+ *  - Veto may be installed in vendor/ as a dependency of another project.
+ */
+
+$autoloadersToTry = array(
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php'
+);
+
+foreach ($autoloadersToTry as $autoloader) {
+    if (file_exists($autoloader)) {
+        $loader = require $autoloader;
+        break;
+    }
+}
+
+if (!$loader) {
+    print 'Unable to find a suitable autoload.php' . PHP_EOL;
+}
+
 $loader->add('Veto\\', __DIR__);
